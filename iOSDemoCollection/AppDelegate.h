32b5750562c206a899e9f64e419c2d6ec6d866e0
zhangjiang1203 <896884553@qq.com>
2018-05-07 11:26:43 +0800

    Initial Commit

---
+++ iOSDemoCollection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSDemoCollection
+//
+//  Created by zitang on 2018/5/7.
+//  Copyright © 2018年 zitang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
